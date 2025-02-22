10998cc37a89255839364ed42596c78c7c0fbb6b
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 16:05:02 +0530

    basic overview of the course

---
+++ 1. Backtracking Course Overview/main.cpp
@@ -0,0 +1,26 @@
+/*
+basics
+
+identification
+
+generalisation
+
+problems
+
+Permutations
+N queens
+Sudoku solver
+
+controlled recusrion
+
+pass by value and pass by reference 
+
+we don't have to apply much brainn into algorithm
+
+data structures > algorithm
+
+most difficul ts Array in terms of diffciculty of problems
+
+
+*/
+
