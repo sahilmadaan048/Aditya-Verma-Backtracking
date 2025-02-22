11dad2d19bb6b9382d94c0149baddb959ed30b50
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 18:01:53 +0530

    Backtracking problems

---
+++ 5. Backtracking problems/main.cpp
@@ -0,0 +1,16 @@
+LEVEL 1
+
+1. Permutations of a strinf
+2. Largest number in kth swap
+3. N digits # with digits in increasinf order
+
+LEVEL 2
+
+4. Rat in a maze
+5. Word break
+6. Palindrome partitioning
+7. Combination Sum
+8. Sudoku solver
+9. N queens
+10. Phone Letter digis 
+11. M-coloring
