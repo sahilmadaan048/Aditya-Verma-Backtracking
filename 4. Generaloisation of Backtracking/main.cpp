b4661809cc6e0c0ddce6abc02e875eaaa2171194
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 17:57:57 +0530

    Generalisation of Backtracking using pseudo code

---
+++ 4. Generaloisation of Backtracking/main.cpp
@@ -0,0 +1,46 @@
+/*
+
+Recap of previous toopic is very important
+
+recursion is the parent of Backtracking
+thus the base conditipns we haver to write here also
+
+generally made void
+
+if(isSolved()) => returns true or false
+
+
+no of choices in backtracking is usually large
+
+controlled recursion
+
+say if choice == S or W only then procees
+
+this type of control in recusrion is usualyy seen in backtracking
+
+
+pass by value/ reference is to be kept in mind while doing backtracking
+
+
+
+
+*/
+
+void solve*&v() {
+
+    if(issolved()) {
+        print/ save
+    }
+
+    for (ch in choices) {
+        if(isvalid == true){
+            choice is _INLINE_VARIABLES_SUPPORTED
+            solve(v)
+            revert changes in v   //this is backtracking
+        }
+    }
+    return
+}
+ 
+
+
