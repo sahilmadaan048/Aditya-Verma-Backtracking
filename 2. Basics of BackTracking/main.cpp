1980e3f40e4d46ed90eb5977f4bbec1438832b27
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 16:48:46 +0530

    Basics of backreacking covered with explanation of pass by value and reference

---
+++ 2. Basics of BackTracking/main.cpp
@@ -0,0 +1,35 @@
+/*
+RECURSIONS :
+
+what is not backtracking ?
+    not really what you think it is
+
+difference between backtracking , dp, and recursion
+    recursion ke hi bacche hain sab
+    backtracking, DP and DnC all
+
+Generic => in recursion you make a recursive tree and find the answer on the leaf node
+        => DP => find optimal solution and gernerally find answer in the leaf node or n, n cell in the tabulation form
+              => is Recursion + memory
+        => BT => recursion + control  
+              => they ask us find all the possible combinations, or permuations
+              => path is the ans
+
+pass by value and pass by reference
+    string s = "Ab" is stored simwhere in the memory
+    pass by value => another string is created when passed through a function instead of the oproginal string
+            changes done to the passed strinf inside the function are not reflected in the original string
+
+    pass by reference => reference of the same string is passed thius changes done to the passed stroing are reflecte in the original string also
+
+BT (backtracking)  = controlled recursion +  pass by reference
+    =>here we take those decisions / choices which are of befinit to us 
+    =>not all of them unlike
+
+backtracking ques can be solved by recursion only and
+vice versa
+ 
+    
+    
+    
+*/ 
