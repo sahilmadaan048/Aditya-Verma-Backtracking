3814d8ad7d951e3f1e86aa88a8ed0068b52c0bbc
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-23 15:56:41 +0530

    unique permutations of strings donr using backtracking

---
+++ 8. Pemutation of strings using backtracking/main.cpp
@@ -0,0 +1,156 @@
+/*
+
+do the problem done in foler no 6
+to find all th epermutation of strings using backtracking
+
+
+
+
+Given a string S. The task is to print all unique permutations of the given string in lexicographically sorted order.
+
+Example 1:
+
+Input: ABC
+Output:
+ABC ACB BAC BCA CAB CBA
+Explanation:
+Given string ABC has permutations in 6
+forms as ABC, ACB, BAC, BCA, CAB and CBA .
+Example 2:
+
+Input: ABSG
+Output:
+ABGS ABSG AGBS AGSB ASBG ASGB BAGS
+BASG BGAS BGSA BSAG BSGA GABS GASB
+GBAS GBSA GSAB GSBA SABG SAGB SBAG
+SBGA SGAB SGBA
+Explanation:
+Given string ABSG has 24 permutations.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function find_permutation() which takes the string S as input parameter and returns a vector of string in lexicographical order.
+
+Expected Time Complexity: O(n! * n)
+Expected Space Complexity: O(n! * n)
+
+
+*/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(v.begin(), v.end())         // sort
+#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
+#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
+#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed
+#define bin(x, y) bitset<y>(x)
+using namespace std;
+int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
+
+void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
+void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
+void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
+// ================================== take ip/op like vector,pairs directly!==================================
+template <typename typC, typename typD>
+istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
+template <typename typC>
+istream &operator>>(istream &cin, vector<typC> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+    return cin;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const pair<typC, typD> &a) { return cout << a.first << ' ' << a.second; }
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a)
+{
+    for (auto &x : a)
+        cout << x << '\n';
+    return cout;
+}
+template <typename typC>
+ostream &operator<<(ostream &cout, const vector<typC> &a)
+{
+    int n = a.size();
+    if (!n)
+        return cout;
+    cout << a[0];
+    for (int i = 1; i < n; i++)
+        cout << ' ' << a[i];
+    return cout;
+}
+// ===================================END Of the input module ==========================================
+
+// pass the input string by reference for backtracking
+void permutations(vector<string> &ans, string &ip, int ind, int n)
+{
+    if (ind == n - 1)
+    {
+        ans.push_back(ip);
+        return;
+    }
+    //unordered_set makes sure that inly unique permutations get prnted
+    //this is important to undersand that if already present in the set. neglect that recursive tree simmce that 
+    //will give the same permutations as earlier
+    //this duplicates strings are eliminated
+    unordered_set<char> st;
+    for (int i = ind; i < n; i++)
+    {
+        if (!st.count(ip[i]))
+        {
+            st.insert(ip[i]);
+
+            swap(ip[i], ip[ind]);
+            permutations(ans, ip, ind + 1, n);
+            swap(ip[i], ip[ind]);
+        }
+    }
+}
+
+void solve()
+{
+    string s = "aab";
+    int n = s.size();
+    vector<string> ans;
+    string curr = "";
+    permutations(ans, s, 0, n);
+
+    for (auto &s : ans)
+        cout << s << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
+/*
+time cimplexity => tc(node) * no of nodes
+
+no of nodes is n!
+tc of each node is o(n)
+swwap = o(1) if store in map i(lig n)
+
+*/
