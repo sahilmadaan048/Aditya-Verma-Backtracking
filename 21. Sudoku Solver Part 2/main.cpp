ba9a9f1499a115b9a14f4cd1924fea93eaa064b0
Sahil Madaan <madaan.sahil27@gmail.com>
2025-04-23 23:43:34 +0530

    ✌  sudoku solver part 2

---
+++ 21. Sudoku Solver Part 2/main.cpp
@@ -0,0 +1,142 @@
+// https://www.youtube.com/watch?v=MlZMrHZCtb8&list=PL_z_8CaSLPWdbOTog8Jxk9XOjzUs3egMP&index=21
+/*
+
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+
+ 
+
+Example 1:
+
+
+Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+
+*/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+const int n = 9;
+vector<vector<int>> sudoku(n, vector<int>(9, 0));
+
+bool isValid(int row, int col, int num) {
+    ///check row
+    for(int i=0; i<n; i++) {
+        if(sudoku[i][col] == num) {
+            return false;
+        }
+    }
+
+    //check column
+    for(int i=0; i<n; i++) {
+        if(sudoku[row][i] == num) {
+            return false;
+        }
+    }
+
+    //check 3 x 4 grids
+    int startRow = row - row%3;
+    int startCol = col - col%3;
+    for(int i=0; i<3; i++) {
+        for(int j=0 ;j<3; j++) {
+            if(sudoku[startRow+i][startCol+j] == num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool solveSudoku() {
+    for(int row = 0; row < n; row++) {
+        for(int col = 0; col < n; col++) {
+            if(sudoku[row][col] == 0) {
+                for(int num = 1; num <= 9; num++) {
+                    if(isValid(row, col, num)) {
+                        sudoku[row][col] = num;
+                        if(solveSudoku()) return true;
+                        sudoku[row][col] = 0;
+                    }
+                }
+                return false; // backtrack
+            }
+        }
+    }
+    return true; // board solved
+}
+
+void solve(){
+    if(solveSudoku()) {
+        for(auto row: sudoku) {
+            for(auto val: row) {
+                cout << val << " ";
+            }
+            cout << endl;
+        }
+    }
+    else {
+        cout << "No solution exists\n";
+    }   
+    return;
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
+/*
+here we are doing cell be cell  traversal
+
+*/
